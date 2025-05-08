c9108a1e7abc81aa1086609dc9385db9fc33067e
Miro8D <miro8d@gmail.com>
2025-05-08 18:08:38 +0300

    Took code from DDNet's website to make the Tee render correctly

--- main.js
+++ main.js
@@ -1,62 +1,37 @@
 const canvas = document.getElementById("skin");
 const ctx = canvas.getContext("2d");
 
-const img = new Image();
-img.src = "assets/default.png";
-img.onload = () => {
+const skin = new Image();
+skin.src = "assets/default.png";
+skin.onload = () => {
 
-    // Drawing the black circle (skin's background ig)
-    ctx.drawImage(img,
-        96, 0, 96, 96,
-        0, 0, 96, 96
-    );
 
-    //Drawing one leg before the body so it appears on the other side
-    //Left leg
-    ctx.drawImage(img, 
-        192, 64, 64, 32,
-        -8, 46, 90, 46
-    );
+    // Code from DDNet's website
 
-    ctx.drawImage(img, 
-        192, 32, 64, 32,
-        -8, 46, 90, 46
-    );
+	// scale
+	const s = skin.naturalWidth / 256
 
-    // Right leg
-    ctx.drawImage(img, 
-        192, 64, 64, 32,
-        17, 46, 90, 46
-    );
+	canvas.width = 96 * s;
+	canvas.height = 64 * s;
+	let ctx = canvas.getContext("2d");
+	canvas.setAttribute("isrenderedskin", 1);
 
-    // Drawing the tee's body
-    ctx.drawImage(img,
-        0, 0, 96, 96,
-        0, 0, 96, 96
-    );
-    
-    // Right leg
+	ctx.drawImage(skin, 192 * s, 64 * s, 64 * s, 32 * s, 8 * s, 32 * s, 64 * s, 30 * s) //back feet shadow
+	ctx.drawImage(skin, 96 * s, 0 * s, 96 * s, 96 * s, 16 * s, 0 * s, 64 * s, 64 * s) //body shadow
+	ctx.drawImage(skin, 192 * s, 64 * s, 64 * s, 32 * s, 24 * s, 32 * s, 64 * s, 30 * s) //front feet shadow
+	ctx.drawImage(skin, 192 * s, 32 * s, 64 * s, 32 * s, 8 * s, 32 * s, 64 * s, 30 * s) //back feet
+	ctx.drawImage(skin, 0 * s, 0 * s, 96 * s, 96 * s, 16 * s, 0 * s, 64 * s, 64 * s) //body
+	ctx.drawImage(skin, 192 * s, 32 * s, 64 * s, 32 * s, 24 * s, 32 * s, 64 * s, 30 * s) //front feet
+	ctx.drawImage(skin, 64 * s, 96 * s, 32 * s, 32 * s, 39 * s, 18 * s, 26 * s, 26 * s) //left eye
 
-    ctx.drawImage(img, 
-        192, 32, 64, 32,
-        17, 46, 90, 46
-    );
-
-    // Eyes
-
-    ctx.drawImage(img, 
-        64, 96, 32, 32,
-        43, 25, 40, 40
-    );
-
-    ctx.drawImage(img, 
-        64, 96, 32, 32,
-        31, 25, 40, 40
-    );
+	//right eye (flip and draw)
+	ctx.save();
+	ctx.scale(-1, 1);
+	ctx.drawImage(skin, 64 * s, 96 * s, 32 * s, 32 * s, -73 * s, 18 * s, 26 * s, 26 * s)
+	ctx.restore();s
 };
 const pngData = canvas.toDataURL("image/png"); // returns a base64 PNG string
 
 const link = document.createElement("a");
 link.href = pngData;
 link.download = "canvas.png";
-link.click();
